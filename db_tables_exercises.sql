f886cb9fb67a859c512a118b8239e083f3f52252
Saul Gonzalez <saul.n.gonzalez3@gmail.com>
2023-09-06 16:03:48 -0500

    updated answers for exercise

--- db_tables_exercises.sql
+++ db_tables_exercises.sql
@@ -18,17 +18,29 @@ SELECT DATABASE();
 SHOW TABLES;
 -- Explore the employees table. What different data types are present in this table?
 DESCRIBE SOURCE employees;
+-- A) int, date, varchart(14), varchart(16), enum, date
 -- Which table(s) do you think contain a numeric type column? (Write this question and your answer in a comment)
--- Salaries
+-- A) dept_emp, dept_manager, employees, salaries, titles
 -- Which table(s) do you think contain a string type column? (Write this question and your answer in a comment)
--- ALL TABLES except for salaries
+-- A) ALL TABLES except for salaries
 -- Which table(s) do you think contain a date type column? (Write this question and your answer in a comment)
--- titles, salaries, employees, dept_manager
+-- A) titles, salaries, employees, dept_manager
 -- What is the relationship between the employees and the departments tables? (Write this question and your answer in a comment)
-SHOW CREATE TABLE employees;
-SHOW CREATE TABLE departments;
--- I could not find a relationship between the two different tables.
+DESCRIBE employees;
+DESCRIBE departments;
+DESCRIBE dept_emp;
+-- A) They are in the same database. The belong to the joiner table dept_emp(which has emp_no and dept_no, each present in employees and departments respectively).
 -- Show the SQL code that created the dept_manager table. Write the SQL it takes to show this as your exercise solution.
+-- A) CREATE TABLE `dept_manager` (
+ --  `emp_no` int NOT NULL,
+--   `dept_no` char(4) NOT NULL,
+--   `from_date` date NOT NULL,
+--   `to_date` date NOT NULL,
+--   PRIMARY KEY (`emp_no`,`dept_no`),
+--   KEY `dept_no` (`dept_no`),
+--   CONSTRAINT `dept_manager_ibfk_1` FOREIGN KEY (`emp_no`) REFERENCES `employees` (`emp_no`) ON DELETE CASCADE ON UPDATE RESTRICT,
+--   CONSTRAINT `dept_manager_ibfk_2` FOREIGN KEY (`dept_no`) REFERENCES `departments` (`dept_no`) ON DELETE CASCADE ON UPDATE RESTRICT
+-- ) ENGINE=InnoDB DEFAULT CHARSET=latin1 
 SHOW CREATE TABLE dept_manager;
 
 
