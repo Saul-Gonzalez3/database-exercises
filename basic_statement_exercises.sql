27c4c1e684061d0b459f741d8c90d9739a55137b
Saul Gonzalez <saul.n.gonzalez3@gmail.com>
2023-09-07 09:25:10 -0500

    completed exercise

--- basic_statement_exercises.sql
+++ basic_statement_exercises.sql
@@ -10,12 +10,25 @@ SELECT * FROM albums;
 -- What do you think the sales column represents?
 -- A) I'm sure that it represents albums sold by the millions.
 -- Find the name of all albums by Pink Floyd.
-SELECT name, artist FROM albums WHERE name GROUP BY artist;
-
+SELECT * FROM albums;
+SELECT name FROM albums WHERE artist = "Pink Floyd";
+-- A) The Dark Side of the Moon and The Wall 
 -- What is the year Sgt. Pepper's Lonely Hearts Club Band was released?
+SELECT * FROM albums;
+SELECT release_date FROM albums WHERE name = "Sgt. Pepper's Lonely Hearts Club Band";
+-- A) The release year was 1967. 
 
 -- What is the genre for the album Nevermind?
+SELECT * FROM albums;
+SELECT genre FROM albums WHERE name = "Nevermind";
+-- A) The genre is Grunge, Alternative rock 
 
 -- Which albums were released in the 1990s?
-
+SELECT * FROM albums;
+SELECT name FROM albums WHERE release_date > 1989 AND release_date < 2000;
+-- A) The albums were: The Bodyguard, Jagged Little Pill, Come On Over, Falling into You, Let's Talk About Love, Dangerous, The Immaculate Collection, Titanic: Music from the Motion Picture, Metallica, Nevermind, and Supernatural.
+ 
 -- Which albums had less than 20 million certified sales? Rename this column as low_selling_albums.
+SELECT * FROM albums;
+SELECT name AS low_selling_albums FROM albums WHERE sales < 20.0;
+-- A) The albums were: Grease, Bad, Sgt. Pepper's Lonely Hearts Club Band, Dirty Dancing, Let's Talk About Love, Dangerous, The Immaculate Collection, Abbey Road, Born in the U.S.A, Brothers in Arms, Titanic, Nevermind, The Wall. 
