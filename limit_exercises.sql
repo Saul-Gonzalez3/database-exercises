b172813e0a0777242866aef6c32373fc1ce5a18e
Saul Gonzalez <saul.n.gonzalez3@gmail.com>
2023-09-08 09:30:15 -0500

    updated exercise

--- limit_exercises.sql
+++ limit_exercises.sql
@@ -5,11 +5,17 @@ SELECT DATABASE();
 SELECT DISTINCT last_name FROM employees ORDER BY last_name DESC LIMIT 10;
 -- List the first 10 distinct last name sorted in descending order .
 -- A) Zykh, Zyda, Zwicker, Zweizig, Zumaque, Zultner, Zucker, Zuberek, Zschoche, Zongker
+
+
 -- Find all previous or current employees hired in the 90s and born on Christmas. Find the first 5 employees hired in the 90's by sorting by hire date and limiting your results to the first 5 records. Write a comment in your code that lists the five names of the employees returned.
 SELECT first_name, last_name, hire_date FROM employees WHERE (hire_date BETWEEN '1990-01-01' AND '1999-12-31') AND birth_date LIKE '%12-25' ORDER BY hire_date LIMIT 5;
 -- A) Alselm Cappello, Utz Mandell, Bouchung Schreiter, Baocai Kushner, Petter Stroustrup
+
+
 -- Try to think of your results as batches, sets, or pages. The first five results are your first page. The five after that would be your second page, etc. Update the query to find the tenth page of results.
 SELECT first_name, last_name, hire_date FROM employees WHERE (hire_date BETWEEN '1990-01-01' AND '1999-12-31') AND birth_date LIKE '%12-25' ORDER BY hire_date LIMIT 5 OFFSET 45;
 -- A) Pranay Narwekar, Marjo Farrow, Ennio Karcich, Dines Lubachevsky, Ipke Fontan
+
+
 -- LIMIT and OFFSET can be used to create multiple pages of data. What is the relationship between OFFSET (number of results to skip), LIMIT (number of results per page), and the page number?
 -- A) The relationship is this: the offset states how many "pages" you skip in regards to each line or name. So for each 5 lines I skipped, it was akin to one page. Two page = 10 lines, three pages = 15 lines, etc.
--- order_by_exercises.sql
+++ order_by_exercises.sql
@@ -4,22 +4,30 @@
 SELECT * FROM employees;
 SELECT first_name, last_name, emp_no  FROM employees WHERE first_name IN ('Irena', 'Vidya', 'Maya') ORDER BY first_name;
 -- A) First was Irena Reutenauer and Last was Vidya Simmen.
+
+
 -- Find all employees with first names 'Irena', 'Vidya', or 'Maya', and order your results returned by first name and then last name. In your comments, answer: What was the first and last name in the first row of the results? What was the first and last name of the last person in the table?
 SELECT first_name, last_name, emp_no  FROM employees WHERE first_name IN ('Irena', 'Vidya', 'Maya') ORDER BY first_name, last_name;
 -- A) First: Irena Acton, Second: Vidya Zweizig
+
+
 -- Find all employees with first names 'Irena', 'Vidya', or 'Maya', and order your results returned by last name and then first name. In your comments, answer: What was the first and last name in the first row of the results? What was the first and last name of the last person in the table?
 SELECT first_name, last_name, emp_no  FROM employees WHERE first_name IN ('Irena', 'Vidya', 'Maya') ORDER BY last_name, first_name;
 -- A) First: Irena Acton, Second: Maya Zyda
+
 -- Write a query to find all employees whose last name starts and ends with 'E'. Sort the results by their employee number. Enter a comment with the number of employees returned, the first employee number and their first and last name, and the last employee number with their first and last name.
-SELECT DISTINCT last_name, first_name, emp_no FROM employees WHERE last_name LIKE 'E%' ORDER BY emp_no;
--- A) Number unknown, First employee (10021 - Ramzi Erde), Last employee (51050 - Parto Egerstedt)
+SELECT DISTINCT last_name, first_name, emp_no FROM employees WHERE (last_name LIKE 'E%' AND last_name LIKE '%e') ORDER BY emp_no;
+-- A) 899, First employee (10021 - Ramzi Erde), Last employee (51050 - Parto Egerstedt)
+
+
 -- Write a query to find all employees whose last name starts and ends with 'E'. Sort the results by their hire date, so that the newest employees are listed first. Enter a comment with the number of employees returned, the name of the newest employee, and the name of the oldest employee.
 SELECT * FROM employees;
 SELECT last_name, first_name, hire_date FROM employees WHERE (last_name LIKE 'E%' AND last_name LIKE '%e') ORDER BY hire_date DESC;
 -- A) There were 899 employees returned. Newest employee hired is Teiji Eldridge and oldest employee hired is Sergi Erde.
+
 -- Find all employees hired in the 90s and born on Christmas. Sort the results so that the oldest employee who was hired last is the first result. Enter a comment with the number of employees returned, the name of the oldest employee who was hired last, and the name of the youngest employee who was hired first.
 SELECT first_name, last_name, hire_date, birth_date FROM employees WHERE (hire_date BETWEEN '1990-01-01' AND '1999-12-31') AND birth_date LIKE '%12-25' ORDER BY birth_date ASC, hire_date DESC;
--- A) There were 363 employees returned. Oldest employee who was hired last (Bernini Khun) and the name of the youngest employee who was hired first (Pettis Douadi).
+-- A) There were 362 employees returned. Oldest employee who was hired last (Khun Bernini) and the name of the youngest employee who was hired first (Douadi Pettis ).
 
 
 SHOW DATABASES;
