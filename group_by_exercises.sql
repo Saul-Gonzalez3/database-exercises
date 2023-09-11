b27e1fb1e10241d8e9395adeced3c846ec407951
Saul Gonzalez <saul.n.gonzalez3@gmail.com>
2023-09-11 06:21:13 -0500

    updated answers to exercise

--- group_by_exercises.sql
+++ group_by_exercises.sql
@@ -15,19 +15,24 @@ SELECT DISTINCT last_name AS UniqueLastName, first_name FROM employees WHERE las
 
 
 -- Write a query to find the unique last names with a 'q' but not 'qu'. Include those names in a comment in your sql code.
-SELECT DISTINCT last_name FROM employees WHERE last_name LIKE '%q%' AND last_name NOT LIKE '%qu%';
+SELECT DISTINCT last_name FROM employees WHERE last_name LIKE '%q%' AND last_name NOT LIKE '%qu%' GROUP BY last_name;
+-- A) Chieq, Linqvist, Qiwen
 
 
 -- Add a COUNT() to your results for exercise 5 to find the number of employees with the same last name.
 
-
+SELECT COUNT(last_name) FROM employees WHERE last_name LIKE '%q%' AND last_name NOT LIKE '%qu%' GROUP BY last_name;
 
 -- Find all employees with first names 'Irena', 'Vidya', or 'Maya'. Use COUNT(*) and GROUP BY to find the number of employees with those names for each gender.
 
-
+SELECT * FROM employees;
+SELECT first_name, gender, count(gender) as HowMany FROM employees WHERE first_name IN ('Irena', 'Vidya', 'Maya') GROUP BY first_name, gender ORDER BY first_name;
 
 -- Using your query that generates a username for all employees, generate a count of employees with each unique username.
 
-
+SELECT * FROM employees;
+SELECT last_name, COUNT(last_name), CONCAT(SUBSTR(LOWER(first_name), 1, 1), SUBSTR(LOWER(last_name), 1, 4), '_', SUBSTR(birth_date, 6, 2), SUBSTR(birth_date, 3, 2)) AS username FROM employees GROUP BY last_name, username;
 
 -- From your previous query, are there any duplicate usernames? What is the highest number of times a username shows up? Bonus: How many duplicate usernames are there?
+
+-- A) Yes there are duplicate usernames, as the count show numerous usernames where there are 2's and even a 3 count (the highest number) appearing in the results. There were 63 duplicate usernames.
